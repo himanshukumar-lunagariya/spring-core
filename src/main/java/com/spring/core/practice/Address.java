3029587ceaf3b72ae4985e880b815ab7561df721
himanshukumarlunagariya <himanshupatel9135@gmail.com>
2026-04-21 00:45:15 -0400

    Initial commit

---
+++ src/main/java/com/spring/core/practice/Address.java
@@ -0,0 +1,50 @@
+package com.spring.core.practice;
+
+public class Address {
+    private String street;
+    private String city;
+    private String province;
+    private String country;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", province='" + province + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/spring/core/practice/App.java
@@ -0,0 +1,18 @@
+package com.spring.core.practice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        Student student = context.getBean("student1", Student.class);
+        System.out.println(student);
+    }
+}
